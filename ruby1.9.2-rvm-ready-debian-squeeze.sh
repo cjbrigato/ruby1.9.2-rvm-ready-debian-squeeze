293301da87b249ee6f82625a7d1b87dd203ccc14
khemael <khemael@hom91-191-151-126.reverse.khemael.net>
2011-12-18 18:09:12 -0500

    For root install... beware the SOURCE for rvm script

--- ruby1.9.2-rvm-ready-debian-squeeze.sh
+++ ruby1.9.2-rvm-ready-debian-squeeze.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-apt-get update
+sudo apt-get update
 
-apt-get -y -V install autoconf automake autotools-dev build-essential bison bzip2 curl git libreadline5 libsqlite3-0 sqlite3 libsqlite3-dev libxml2-dev libmysqlclient-dev libreadline5-dev libruby openssl libssl-dev zlib1g zlib1g-dev zlibc git-core gcc make libxml2-dev libxslt-dev libopenssl-ruby libncurses5-dev nginx libapr1-dev libaprutil1-dev build-essential libcurl4-openssl-dev
+sudo apt-get -y -V install autoconf automake autotools-dev build-essential bison bzip2 curl git libreadline5 libsqlite3-0 sqlite3 libsqlite3-dev libxml2-dev libmysqlclient-dev libreadline5-dev libruby openssl libssl-dev zlib1g zlib1g-dev zlibc git-core gcc make libxml2-dev libxslt-dev libopenssl-ruby libncurses5-dev nginx libapr1-dev libaprutil1-dev build-essential libcurl4-openssl-dev
   
 bash < <( curl -L -B http://rvm.beginrescueend.com/install/rvm )
 
 echo '[[ -s "$HOME/.rvm/scripts/rvm" ]] && . "$HOME/.rvm/scripts/rvm"' >> .bashrc
+#IF YOU  RAN THIS INSTALL AS ROOT :
+#echo [[ -s "/usr/local/rvm/scripts/rvm" ]] && . "/usr/local/rvm/scripts/rvm" >> .bashrc
 
 rvm package install zlib openssl
 
